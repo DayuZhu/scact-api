4c67202b9988e04b4e02a1c4cf53ed4291424d78
zhuxiaoyu <zhuxiaoyu@zhiketong.cn>
2020-01-13 20:12:34 +0800

    刪除front 和 shopxo

--- shopxo/public/static/admin/default/js/pluginsadmin.js
+++
@@ -1,9 +0,0 @@
-$(function()
-{
-    $('.submit-delete-modal').on('click', function()
-    {
-        var $modal = $('#plugins-delete-modal');
-        $modal.find('button.submit-ajax').attr('data-id', $(this).data('id'));
-        $modal.modal('open');
-    });
-});
