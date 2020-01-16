a46ec1bec2e1592e445d3c0e5e702242aa432409
zhuxiaoyu <zhuxiaoyu@zhiketong.cn>
2020-01-16 14:35:28 +0800

    删除不用的注解

--- src/main/java/com/sc/act/api/service/impl/ActivityWinnersProductServiceImpl.java
+++ src/main/java/com/sc/act/api/service/impl/ActivityWinnersProductServiceImpl.java
@@ -15,7 +15,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -72,7 +71,6 @@ public class ActivityWinnersProductServiceImpl implements ActivityWinnersProduct
      * @param currentTime
      * @param productPriceInfoList
      */
-    //@Transactional(rollbackFor = Exception.class)
     @Override
     public void createWinnersProductInfo(List<ExcelWinnersInfoBmo> list, Activity activity, Date currentTime, List<ProductPriceInfoBmo> productPriceInfoList) {
 
